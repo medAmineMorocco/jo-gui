7e8f71eb982cfb6f148a9d3e57c33253ac70c34f
afromy <afromy@norsys.fr>
2020-08-11 17:41:23 +0200

    Correction responsive title

--- src/components/title/StyledTitle.js
+++ src/components/title/StyledTitle.js
@@ -1,16 +1,16 @@
 import React from "react";
-import { Row, Col } from 'antd';
 import "./styledTitle.css";
+import { Row, Col } from 'antd';
 import { useWindowSize } from "@hooks/window";
 
 export function StyledTitle({ title1, title2, style }) {
     const isMobile = useWindowSize();
     return (
-        <Row align="middle" justify="center">
-        <h1 style={style} className="styled-title-container">
+	<Row justify="center" align="middle" >
+        <h1 style={!isMobile ? style : {}} className="styled-title-container">
             <Col span={24}>{title1}</Col> 
             <Col span={24}>{title2}</Col>
         </h1>
-        </Row>
+    </Row>
     );
 }
