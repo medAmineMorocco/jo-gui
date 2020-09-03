e104f823d0e797f543e4b70791d6bdbb5101d828
Ammach Mohamed Amine <maammach@norsys.fr>
2020-09-03 12:20:38 +0100

    refactor(Summary): import svg as components

--- .storybook/main.js
+++ .storybook/main.js
@@ -16,6 +16,13 @@ module.exports = {
             '@theme': path.resolve(__dirname, '../src/theme/')
         };
 
+        const fileLoaderRule = config.module.rules.find(rule => rule.test.test('.svg'));
+        fileLoaderRule.exclude = /\.svg$/;
+        config.module.rules.push({
+            test: /\.svg$/,
+            use: ["@svgr/webpack", "url-loader"],
+        });
+
 
         return config;
     },
--- .storybook/stories/summary.stories.js
+++ .storybook/stories/summary.stories.js
@@ -1,13 +1,16 @@
 import React from 'react';
 import { withKnobs } from "@storybook/addon-knobs";
+import {Summary} from "@components/summary/Summary";
 import {DynamicSummary} from "@components/dynamicSummary/DynamicSummary";
 import {persoItems} from '@utils/category';
 
-export default { title: 'DynamicSummary', decorators: [withKnobs]};
+export default { title: 'Summary & DynamicSummary', decorators: [withKnobs]};
 
-export const summaryBeforeIStart = () => <DynamicSummary items={persoItems} />;
+export const summary = () => <Summary items={persoItems} />;
 
-export const summaryWhenIProcessFirstForm = () => <DynamicSummary items={persoItems} current={0} />;
+export const dynamicSummaryBeforeIStart = () => <DynamicSummary items={persoItems} />;
 
-export const summaryWhenICompleteTwoForms = () => <DynamicSummary items={persoItems} current={2} />;
+export const dynamicSummaryWhenIProcessFirstForm = () => <DynamicSummary items={persoItems} current={0} />;
+
+export const dynamicSummaryWhenICompleteTwoForms = () => <DynamicSummary items={persoItems} current={2} />;
 
