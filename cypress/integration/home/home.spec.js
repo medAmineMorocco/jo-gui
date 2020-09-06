fc2c5553c909c6a3e1efafbef93b155892098e1d
Mohamed Amine AMMACH <maammach@norsys.fr>
2020-09-06 20:09:57 +0200

    config(CI): configure visual and functional tests on CI

---
+++ cypress/integration/home/home.spec.js
@@ -0,0 +1,30 @@
+context('Home page', () => {
+
+    const sizes = [
+        {
+            device: 'iphone-5',
+            width: 320
+        },
+        {
+            device: 'ipad-2',
+            width: 768
+        },
+        {
+            device: 'macbook-13',
+            width: 1280
+        }
+    ];
+
+
+    it('should show home page', () => {
+        sizes.forEach(size => {
+            cy.viewport(size.device);
+
+            cy.login('email@paris2024.org');
+
+            cy.url().should('include', '/home').then(() => {
+                cy.takeSnapshots('home page', size);
+            });
+        })
+    });
+});
--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -1,6 +1,8 @@
+import '@percy/cypress';
+
 Cypress.Commands.add("stubRequest", (method, path, status, fixture, alias) => {
     cy.server();
-    if(fixture) {
+    if (fixture) {
         cy.fixture(fixture).as(alias);
     }
     const response = alias ? `@${alias}` : `${status} error`;
@@ -14,9 +16,23 @@ Cypress.Commands.add("stubRequest", (method, path, status, fixture, alias) => {
 
 Cypress.Commands.add('selectOption', (selector, value) => {
     cy.get(selector)
+        .parents('.ant-select-selector')
         .click({force: true})
         .then(() => {
             cy.get(`.ant-select-item-option-content:contains(${value})`).click();
         });
 
+});
+
+Cypress.Commands.add('takeSnapshots', (title, size) => {
+    cy.wait(1000);
+    cy.percySnapshot(`${title} | on ${size.device}`, {widths: [size.width]});
+});
+
+Cypress.Commands.add('login', (email) => {
+    cy.stubRequest('POST', '**/auth/signin', 200, 'signin.json', 'signinJSON');
+    cy.clearLocalStorage();
+    cy.visit('/');
+    cy.get('#login_email').clear().type(email);
+    cy.get('form').submit();
 });
