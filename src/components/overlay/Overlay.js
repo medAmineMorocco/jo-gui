6630590b5be4fad66020ec811ac72a68ccf88fa4
Anthony FROMY <afromy@norsys.fr>
2020-09-24 12:27:17 +0200

    Fix/overlay

--- src/components/overlay/Overlay.js
+++ src/components/overlay/Overlay.js
@@ -17,7 +17,7 @@ export function Overlay(props) {
   let { items } = props;
 
   const titleModal = (
-    <div>
+    <div className="container-flex-title-modal">
       <BulbOutlined className="icon-modal" />
       <p className="title-modal">Comment réduire sont empreinte ?</p>
     </div>
@@ -38,45 +38,54 @@ export function Overlay(props) {
 
   return (
     <>
-      <Button
-        className="custom-btn-modal"
-        type="primary"
-        onClick={() =>
-          SetVisible({ modalVisible: true, closeButton: "inline-block" })
-        }
-      >
-        <BulbOutlined className="icon-modal" />
-        <p className="title-button">Comment réduire sont empreinte ?</p>
-      </Button>
-      <Button
-        className="modal-close-btn-modal"
-        type="primary"
-        shape="circle"
-        icon={<CloseOutlined />}
-        onClick={() => SetVisible({ modalVisible: false, closeButton: "none" })}
-        style={{ display: closeButton }}
-      />
-      <Modal
-        title={titleModal}
-        visible={modalVisible}
-        onOk={() => SetVisible({ modalVisible: false, closeButton: "none" })}
-        onCancel={() =>
-          SetVisible({ modalVisible: false, closeButton: "none" })
-        }
-        className="custom-modal"
-        footer={null}
-        centered
-        closable={false}
-      >
-        <Carousel
-          draggable={true}
-          arrows={true}
-          prevArrow={<LeftOutlined />}
-          nextArrow={<RightOutlined />}
+      <div className="container-flex-overlay">
+        <Button
+          className="custom-btn-modal"
+          type="primary"
+          onClick={() =>
+            SetVisible({ modalVisible: true, closeButton: "inline-block" })
+          }
         >
-          {tiles}
-        </Carousel>
-      </Modal>
+          <div className="container-text-button">
+            <BulbOutlined className="icon-modal" />
+            <span className="title-button">
+              Comment réduire sont empreinte ?
+            </span>
+            <div></div>
+          </div>
+        </Button>
+        <Button
+          className="modal-close-btn-modal"
+          type="primary"
+          shape="circle"
+          icon={<CloseOutlined />}
+          onClick={() =>
+            SetVisible({ modalVisible: false, closeButton: "none" })
+          }
+          style={{ display: closeButton }}
+        />
+        <Modal
+          title={titleModal}
+          visible={modalVisible}
+          onOk={() => SetVisible({ modalVisible: false, closeButton: "none" })}
+          onCancel={() =>
+            SetVisible({ modalVisible: false, closeButton: "none" })
+          }
+          className="custom-modal"
+          footer={null}
+          centered
+          closable={false}
+        >
+          <Carousel
+            draggable={true}
+            arrows={true}
+            prevArrow={<LeftOutlined />}
+            nextArrow={<RightOutlined />}
+          >
+            {tiles}
+          </Carousel>
+        </Modal>
+      </div>
     </>
   );
 }
