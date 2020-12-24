471c56a068cbba8ef3555350ba2b4f9097a7ad94
ammach <maammach@norsys.fr>
2020-12-24 12:45:52 +0100

    fix: services

--- src/pages/form/perso/step7/step7State.js
+++ src/pages/form/perso/step7/step7State.js
@@ -1,15 +1,13 @@
 export const persostep7State = (values) => {
-  return [
-    {
-      thematic: "SERVICES",
-      questions: [
-        {
-          question: "5f557a78e938b",
-          response: 1283.76,
-        },
-      ],
-      actions: [],
-      settings: [],
-    },
-  ];
+  return {
+    thematic: "SERVICES",
+    questions: [
+      {
+        question: "5f557a78e938b",
+        response: 1283.76,
+      },
+    ],
+    actions: [],
+    settings: [],
+  };
 };
