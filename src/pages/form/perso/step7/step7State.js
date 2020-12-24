9ed5b64901405f156ed22010ff52a73c9a65148b
ammach <maammach@norsys.fr>
2020-12-24 11:24:45 +0100

    fix: value in persoStep7

--- src/pages/form/perso/step7/step7State.js
+++ src/pages/form/perso/step7/step7State.js
@@ -5,7 +5,7 @@ export const persostep7State = (values) => {
       questions: [
         {
           question: "5f557a78e938b",
-          response: values["5f557a78e938b"],
+          response: 1283.76,
         },
       ],
       actions: [],
