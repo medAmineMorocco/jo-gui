509cdb769865aa408d5432147d28a364103b045c
Mohamed Amine AMMACH <maammach@norsys.fr>
2021-02-25 21:00:17 +0100

    Fix/reduire

--- src/components/actionsTable/ActionsTable.js
+++ src/components/actionsTable/ActionsTable.js
@@ -27,7 +27,7 @@ export function ActionsTable({
           ({ category, thematic, description, reduction, gain }, key) => {
             return (
               <tr className="table-contents" key={key}>
-                <td className="table-contents-description">${description}`}</td>
+                <td className="table-contents-description">{description}</td>
                 {showGain && (
                   <td className="table-contents-gain">{round(gain)}</td>
                 )}
