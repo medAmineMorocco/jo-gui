19cf2deca34c86febd572b26b664414004e7d64a
afromy <afromy@norsys.fr>
2021-03-02 15:06:43 +0100

    Quick fix error message

--- src/services/authService.js
+++ src/services/authService.js
@@ -18,6 +18,20 @@ export function login(email, password) {
       return json;
     })
     .catch((error) => {
+      switch (error.status) {
+        case 500:
+          error = "Adresse mail ou mot de passe incorrect";
+          break;
+        case 400:
+          error = "Adresse mail ou mot de passe incorrect";
+          break;
+        case 300:
+          error = "Adresse mail ou mot de passe incorrect";
+          break;
+        default:
+          error =
+            "Service momentanement indisponible veuillez réessayer ultérieurement";
+      }
       return Promise.reject(error);
     });
 }
